90ac22851f1a5b8a7fb43498e5c39cca9d455e69
donut.ipynb <25983786+Donifide@users.noreply.github.com>
2021-09-16 16:45:37 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 Config for various platform logins
 """
 import os
+BINANCE_KEY = ""
+BINANCE_SECRET = ""
 
 #Enter your robinhood username and password here, then save this file in the same directory as a regular python file
 rh_password = os.environ.get('rh_pw')
